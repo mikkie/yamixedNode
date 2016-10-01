c72b04951b2cb449f5355f844745012cfb57909c
benjaminl <benjamin.li@ehealth-china.com>
2016-10-01 15:13:56 +0800

    update link

--- routes/link/index.js
+++ routes/link/index.js
@@ -7,21 +7,37 @@ var express = require('express'),
     Link = mongoose.model('Link');
 
 router.post('/postNewLink', function (req, res) {
-    var link = new Link();
-    link.url = req.body.url;
-    link.title = req.body.title;
-    link.description = req.body.description;
-    link.previewImg = req.body.previewImg;
-    link.spaceId = mongoose.Types.ObjectId(req.body.spaceId);
-    link.tags = req.body.tags;
-    link.save(function (err, result) {
-        if (err) {
-            res.json({"error": "??????"});
-        }
-        else {
-            res.json({"success": result});
-        }
-    });
+    var id = req.body._id;
+    if(id){
+        Link.findByIdAndUpdate(mongoose.Types.ObjectId(id),
+            { $set: { title : req.body.title, description : req.body.description,
+                previewImg : req.body.previewImg,tags : req.body.tags,
+                spaceId : mongoose.Types.ObjectId(req.body.spaceId)}}, { new: true }, function (err, doc) {
+                if (err) {
+                    res.json({"error": "更新链接失败" + id});
+                }
+                else {
+                    res.json({"success": doc});
+                }
+        });
+    }
+    else{
+        var link = new Link();
+        link.url = req.body.url;
+        link.title = req.body.title;
+        link.description = req.body.description;
+        link.previewImg = req.body.previewImg;
+        link.spaceId = mongoose.Types.ObjectId(req.body.spaceId);
+        link.tags = req.body.tags;
+        link.save(function (err, result) {
+            if (err) {
+                res.json({"error": "??????"});
+            }
+            else {
+                res.json({"success": result});
+            }
+        });
+    }
 });
 
 router.post('/getLinksBySpace', function (req, res) {
@@ -67,6 +83,29 @@ router.post('/updateLinkVisitTime', function (req, res) {
     });
 });
 
+
+router.post('/deleteLink', function (req, res) {
+    Link.findOneAndRemove({_id: mongoose.Types.ObjectId(req.body.linkId)},function(err){
+        if (err) {
+            res.json({"error": "删除失败" + req.body.linkId});
+        }
+        else {
+            res.json({"success": "删除成功" + req.body.linkId});
+        }
+    });
+});
+
+
+router.post('/getLinkById', function (req, res) {
+    Link.findOne({_id: mongoose.Types.ObjectId(req.body.linkId)},function(err,doc){
+        if (err) {
+            res.json({"error": "获取Link失败" + req.body.linkId});
+        }
+        else {
+            res.json({"success": doc});
+        }
+    });
+});
 
 
 module.exports = router;
