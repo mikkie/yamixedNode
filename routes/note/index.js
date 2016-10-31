122ed12e02e3bfcde00e75a4a551eafb6055a808
benjaminl <benjamin.li@ehealth-china.com>
2016-10-31 15:34:06 +0800

    link-note

--- routes/note/index.js
+++ routes/note/index.js
@@ -14,64 +14,75 @@ router.post('/new', function (req, res) {
             res.json({"error":err});
         }
         else{
-            var detail = {
-                content : req.body.content,
-                sentence : req.body.sentence,
-                x : req.body.x,
-                y : req.body.y
-            };
-            if(docs.length > 0){
-                note = docs[0];
-            }
-            else{
-                note = new Note();
-                note.url = req.body.url;
-                note.owner = mongoose.Types.ObjectId(req.body.owner);
-                note.notes = [];
-            }
-            if(req.body.id && note.notes.length > 0){
-               for(var i = 0; i < note.notes.length; i++){
-                 if(note.notes[i].toObject()._id.toString() == req.body.id){
-                     note.notes[i].content = req.body.content;
-                     note.notes[i].createDate = new Date();
-                     break;
-                 }
-               }
-            }
-            else{
-                createLink(req.body.url,req.body.space,req.body.owner,req.body.sentence,req.body.content);
-                note.notes.push(detail);
-            }
-            note.save(function(err,doc){
-                if(err){
-                    res.json({"error":err});
+            createLink(req.body.url,req.body.space,req.body.owner,req.body.sentence,req.body.content,res,function(link){
+                var detail = {
+                    content : req.body.content,
+                    sentence : req.body.sentence,
+                    space : mongoose.Types.ObjectId(req.body.space),
+                    link : mongoose.Types.ObjectId(link._id),
+                    x : req.body.x,
+                    y : req.body.y
+                };
+                if(docs.length > 0){
+                    note = docs[0];
                 }
                 else{
-                    var noteO = doc.toObject();
-                    var notes = noteO.notes;
-                    notes.sort(function(a,b){
-                        return a.createDate - b.createDate;
-                    });
-                    var detail = notes[notes.length - 1];
-                    detail.url = noteO.url;
-                    detail.owner = noteO.owner;
-                    res.json({"success":detail});
+                    note = new Note();
+                    note.url = req.body.url;
+                    note.owner = mongoose.Types.ObjectId(req.body.owner);
+                    note.notes = [];
                 }
+                if(req.body.id && note.notes.length > 0){
+                    for(var i = 0; i < note.notes.length; i++){
+                        if(note.notes[i].toObject()._id.toString() == req.body.id){
+                            note.notes[i].content = req.body.content;
+                            note.notes[i].createDate = new Date();
+                            break;
+                        }
+                    }
+                }
+                else{
+                    note.notes.push(detail);
+                }
+                note.save(function(err,doc){
+                    if(err){
+                        res.json({"error":err});
+                    }
+                    else{
+                        var noteO = doc.toObject();
+                        var notes = noteO.notes;
+                        notes.sort(function(a,b){
+                            return a.createDate - b.createDate;
+                        });
+                        var detail = notes[notes.length - 1];
+                        detail.url = noteO.url;
+                        detail.owner = noteO.owner;
+                        res.json({"success":detail});
+                    }
+                });
             });
         }
     });
 });
 
 
-var createLink = function(url,space,owner,sentence,content){
+var createLink = function(url,space,owner,sentence,content,res,callback){
     var link = new Link();
     link.url = url;
     link.title = sentence;
     link.description = content;
+    link.valid = true;
     link.previewImg = '';
     link.spaceId = mongoose.Types.ObjectId(space);
     link.owner = mongoose.Types.ObjectId(owner);
-    link.save(function (err, result) {});
+    link.save(function (err, result) {
+        if(err){
+            res.json({"error":err});
+        }
+        else{
+            callback(result.toObject());
+        }
+    });
 };
 
 router.post('/delete', function (req, res) {
@@ -87,6 +98,7 @@ router.post('/delete', function (req, res) {
                         if(note.notes[i].toObject()._id.toString() == req.body.id){
                             note.notes[i].valid = false;
                             note.notes[i].createDate = new Date();
+                            Link.findOneAndRemove({_id:mongoose.Types.ObjectId(note.notes[i].link)},function(err,doc){});
                             break;
                         }
                     }
